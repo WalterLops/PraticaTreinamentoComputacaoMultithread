cb176542c2be50a66eda9940f305b193ed96740e
Walter Lopes <waltermagno02@gmail.com>
2024-05-09 17:59:23 -0300

    Modificações

--- Thread/Exercicio3/Program.cs
+++ Thread/Exercicio3/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Exercicio3;
-
-internal static class Program
+﻿public static class Program
 {
     private static readonly object ControleAcesso = new();
     public static void Main()
     {
-        // ...
+        var n = 20;
+        var listaThreads = new List<Thread>();
+
+        for (var i = 0; i < n; ++i)
+            listaThreads.Add(new Thread(Imprimir));
+
+        for (var i = 0; i < n; ++i)
+            listaThreads[i].Start(i+1);
+
+        Console.WriteLine("Antes de Fim");
+
+        foreach (var t in listaThreads)
+            t.Join();
+
+        Console.WriteLine("Fim");
+    }
+    
+    static void Imprimir(object? param)
+    {
+        var id = (int)(param ?? 0);
+        var caracteres = $"Hello World! {id}";
+        
+        foreach (var caracter in caracteres)
+            Console.Write(caracter);
+        
+        Console.WriteLine();
     }
 }
